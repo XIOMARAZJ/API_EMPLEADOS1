8f443cea3348e02882f53cfd0f088f01f8c167c1
armandoski <monicazj238@gmail.com>
2023-08-01 20:28:31 -0500

    api-empls

--- api_empls.py
+++ api_empls.py
@@ -13,10 +13,10 @@ if response.status_code == 200:
     data = response.json()
 
     # cantidad de empleados
-    total_employees = len(data)
+    total_employees = len(data['data'])  # Corrección aquí
     print(f"CANTIDAD TOTAL DE EMPLEADOS : {total_employees}")
 
-    # promedio de salario
+    # promedio de salario de los empleados
     total_salary = sum(float(employee['employee_salary']) for employee in data['data'])
     average_salary = total_salary / total_employees
     print(f" PROMEDIO DEL SALARIO  : {average_salary}")
@@ -33,14 +33,12 @@ if response.status_code == 200:
     print(f"SALARIO MINIMO: {min_salary}")
     print(f"SALARIO BASICO: {max_salary}")
 
-
     # edad mínima y máxima
     ages = [int(employee['employee_age']) for employee in data['data']]
     min_age = min(ages)
     max_age = max(ages)
     print(f"EDAD MENOR: {min_age}")
     print(f"EDAD MAYOR: {max_age}")
-
 
 else:
     print("Error al obtener los datos de empleados. Código de estado:"), response.status_code
